e588807cce233c78d6d156e04363ae15e739bee7
Inacora <inacorateresacamposalonso@alumno.ieselrincon.es>
2025-05-15 12:32:11 +0100

    cambios

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function create(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+
+    Route::apiResource('plants', PlantController::class);
+
+    Route::apiResource('users', UserController::class);
 });
+
+Route::post('users/create', [UserController::class, 'create'])->name('users.create');
+
+ 
