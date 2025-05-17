1faa4f6646f9c3167696f3ca15b5b44720269a86
Inacora <inacorateresacamposalonso@alumno.ieselrincon.es>
2025-05-17 20:32:58 +0100

    cosas arregladas

--- config/cors.php
+++ config/cors.php
@@ -16,11 +16,10 @@ return [
     */
 
     
-        'paths' => ['api/*', 'sanctum/csrf-cookie'],
+        'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', 'register'],
         'allowed_methods' => ['*'],
         'allowed_origins' => ['http://localhost:4200'], 
         'allowed_origins_patterns' => [],
-        'Access-Control-Allow-Origin' => ['*'],
         'allowed_headers' => ['*'],
         'exposed_headers' => [],
         'max_age' => 0,
